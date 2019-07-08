336cc0921d6db4d8614df23c0323664c9fa76e9f
zeloras <zeloras@gmail.com>
2019-07-08 09:16:31 +0300

    permission to static

--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web', 'permission:admin_access'], 'prefix' => getAdminPrefix('feedback')], function () {
+Route::group(['middleware' => ['web', 'permission:' . \Gcms::MAIN_ADMIN_PERMISSION], 'prefix' => getAdminPrefix('feedback')], function () {
     Route::group(['middleware' => ['permission:modules_feedback_admin_list']], function () {
         Route::get('/', 'GeekCms\Feedback\Http\Controllers\AdminController@index')
             ->name('admin.feedback')
