4104ec5db6de0e658350b9e2f0e7659db6ffad87
zeloras <zeloras@gmail.com>
2019-07-08 19:53:57 +0300

    some fixes for modules

--- Http/routes.php
+++ Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['middleware' => ['web', 'permission:' . \Gcms::MAIN_ADMIN_PERMISSION], 'prefix' => getAdminPrefix('feedback')], function () {
     Route::group(['middleware' => ['permission:modules_feedback_admin_list']], function () {
-        Route::get('/', 'GeekCms\Feedback\Http\Controllers\AdminController@index')
+        Route::get(DIRECTORY_SEPARATOR, 'GeekCms\Feedback\Http\Controllers\AdminController@index')
             ->name('admin.feedback')
         ;
     });
